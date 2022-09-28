babd73acf81f3640f396442d7b9736e77aac95d7
Natália Fonseca <nataliabf@outlook.com>
2022-09-28 13:22:42 -0300

    style: remove useless comments

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -48,56 +48,5 @@ class _HomeScreenState extends State<HomeScreen> {
         child: const Icon(Icons.add),
       ),
     );
-
-    // return Scaffold(
-    //   appBar: AppBar(title: const Text('easyProjects')),
-    //   body: ListView(children: [
-    //     Padding(
-    //       padding: const EdgeInsets.all(16.0),
-    //       child: Row(
-    //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //         children: [
-    //           const Text(
-    //             'Meus Projetos',
-    //             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-    //           ),
-    //           ElevatedButton(
-    //               onPressed: () {
-    //                 // Navigator.pushReplacement(
-    //                 //   context,
-    //                 //   MaterialPageRoute(
-    //                 //     builder: (context) => const LoginScreen(),
-    //                 //   ),
-    //                 // );
-    //                 ProjectsData.of(context)
-    //                     .newProject('title', 'manager', 'imageUrl', 'deadline');
-    //                 setState(() {});
-    //                 print(ProjectsData.of(context).projectList);
-    //               },
-    //               child: const Text('Sair'))
-    //         ],
-    //       ),
-    //     ),
-    //     Column(
-    //       children: ProjectsData.of(context).projectList,
-    //     ),
-    //     const SizedBox(
-    //       height: 100,
-    //     )
-    //   ]),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: () {
-    //       Navigator.push(
-    //         context,
-    //         MaterialPageRoute(
-    //           builder: (contextNew) => FormScreen(
-    //             projectsContext: context,
-    //           ),
-    //         ),
-    //       );
-    //     },
-    //     child: const Icon(Icons.add),
-    //   ),
-    // );
   }
 }
